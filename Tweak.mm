7abe278e1dad4d0ded905b84f9831b27e031ad5d
u9km <132750548+u9km@users.noreply.github.com>
2025-12-28 12:12:04 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -2,48 +2,26 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface EliteGlobalShield : NSObject
+@interface GlobalProtector : NSObject
 @end
 
-@implementation EliteGlobalShield
-// وظائف التزييف الصامتة
-- (id)getFakeID { return [[NSUUID alloc] initWithUUIDString:[[NSUUID UUID] UUIDString]]; }
-- (void)nullify { return; }
-- (bool)fakeStatus { return NO; }
+@implementation GlobalProtector
+// تزييف الهوية
+- (id)fakeUUID { return [[NSUUID alloc] initWithUUIDString:[[NSUUID UUID] UUIDString]]; }
+// تزييف إصدار النظام إلى 12.1 (إصدار قديم ومستقر)
+- (NSString *)fakeOSVersion { return @"12.1"; }
+- (NSString *)fakeModel { return @"iPhone 8"; }
 @end
 
-// 1. حماية ENKOS: منع الوصول لسجلات الذاكرة
-void BypassEnkosSystem() {
-    // إنكوس تبحث عن ملفات التعديل في مجلدات معينة
-    NSString *libPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Library"];
-    NSArray *enkosLists = @[@"en_kos", @"en_config", @"enkos_logs"];
-    
-    NSFileManager *fm = [NSFileManager defaultManager];
-    for (NSString *folder in enkosLists) {
-        [fm removeItemAtPath:[libPath stringByAppendingPathComponent:folder] error:nil];
-    }
-}
-
-// 2. حماية SHADOW: إعاقة تقارير الطرف الثالث
-void DisableShadowAnalytics() {
-    NSArray *classes = @[@"ShadowTrackerExtra", @"TDataMaster", @"SecurityGuard", @"AnticheatManager"];
-    for (NSString *name in classes) {
-        Class cls = objc_getClass([name UTF8String]);
-        if (cls) {
-            // استبدال دالة كشف التعديل
-            Method m = class_getInstanceMethod(cls, sel_registerName("isDetectionTriggered"));
-            if (m) method_setImplementation(m, class_getMethodImplementation([EliteGlobalShield class], @selector(fakeStatus)));
-        }
-    }
-}
-
-// 3. التنظيف العميق (Deep Clean)
-void GlobalDeepClean() {
+// تنظيف ملفات الباند الغيابي
+void PowerClean() {
     NSString *home = NSHomeDirectory();
     NSArray *paths = @[
-        @"Library/Caches", @"Library/Logs", 
+        @"Library/Caches", @"Library/Logs", @"tmp",
         @"Documents/ShadowTrackerExtra/Saved/Logs",
-        @"Documents/ShadowTrackerExtra/Saved/SrcCheck"
+        @"Documents/ShadowTrackerExtra/Saved/Pandora",
+        @"Documents/ShadowTrackerExtra/Saved/SrcCheck",
+        @"Documents/ShadowTrackerExtra/Saved/StatCache"
     ];
     NSFileManager *fm = [NSFileManager defaultManager];
     for (NSString *p in paths) {
@@ -51,30 +29,37 @@ void GlobalDeepClean() {
     }
 }
 
-// محرك الحقن (The Engine)
-__attribute__((constructor)) static void start_elite_protection() {
-    // تنظيف استباقي
-    GlobalDeepClean();
-    BypassEnkosSystem();
-
-    // تأخير 55 ثانية لتخطي كافة طبقات الفحص عند الإقلاع
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(55 * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+void ActivateSpoofing() {
+    Class dev = objc_getClass("UIDevice");
+    if (dev) {
+        // 1. تزييف رقم الإصدار (System Version)
+        method_setImplementation(class_getInstanceMethod(dev, @selector(systemVersion)),
+                                 class_getMethodImplementation([GlobalProtector class], @selector(fakeOSVersion)));
         
-        // تفعيل تزييف الهوية الديناميكي
-        Class dev = objc_getClass("UIDevice");
-        if (dev) {
-            method_setImplementation(class_getInstanceMethod(dev, @selector(identifierForVendor)),
-                                     class_getMethodImplementation([EliteGlobalShield class], @selector(getFakeID)));
-        }
+        // 2. تزييف نوع الجهاز لمنع التدقيق المتقاطع
+        method_setImplementation(class_getInstanceMethod(dev, @selector(model)),
+                                 class_getMethodImplementation([GlobalProtector class], @selector(fakeModel)));
 
-        DisableShadowAnalytics();
+        // 3. تزييف الهوية (IDFV)
+        method_setImplementation(class_getInstanceMethod(dev, @selector(identifierForVendor)),
+                                 class_getMethodImplementation([GlobalProtector class], @selector(fakeUUID)));
+    }
+}
+
+__attribute__((constructor)) static void load() {
+    // تنظيف أولي صامت
+    PowerClean();
+
+    // تأخير 60 ثانية لتجاوز فحوصات الأمان الثقيلة عند البداية
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        // تنظيف دوري صامت كل 100 ثانية لقتل سجلات شادو وإنكوس
-        [NSTimer scheduledTimerWithTimeInterval:100.0 repeats:YES block:^(NSTimer *timer) {
-            GlobalDeepClean();
-            BypassEnkosSystem();
+        ActivateSpoofing();
+        
+        // تنظيف دوري شرس كل 30 ثانية لقتل الباند الغيابي
+        [NSTimer scheduledTimerWithTimeInterval:30.0 repeats:YES block:^(NSTimer *timer) {
+            PowerClean();
         }];
         
-        NSLog(@"[VIP] SHADOW & ENKOS BYPASSED.");
+        NSLog(@"[VIP] SYSTEM SPOOFING & ANTI-BAN ACTIVE.");
     });
 }

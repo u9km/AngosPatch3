8b615f8b95121e1c35515d2a309910df89960b56
u9km <132750548+u9km@users.noreply.github.com>
2025-12-28 18:49:56 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -3,62 +3,57 @@
 #import <objc/runtime.h>
 #import <dlfcn.h>
 
-// تشفير المسارات لمنع الحماية من معرفة ما نقوم بحذفه
-#define CLOUD_PATH @"Documents/ShadowTrackerExtra/Saved/CloudStorage"
-#define STAT_PATH @"Documents/ShadowTrackerExtra/Saved/StatCache"
-
-@interface NewSecurityBypass : NSObject
+@interface BlackAmarShield : NSObject
+- (id)fetchNewID;
+- (void)silentMode;
 @end
 
-@implementation NewSecurityBypass
-- (id)initDynamic { return [[NSUUID UUID] UUIDString]; }
-- (void)stop { return; }
+@implementation BlackAmarShield
+- (id)fetchNewID {
+    return [[NSUUID UUID] UUIDString];
+}
+- (void)silentMode { return; }
 @end
 
-// تنظيف "صامت" للمسارات التي استحدثتها ببجي في التحديث الأخير
-void SilentPurge() {
-    NSString *h = NSHomeDirectory();
+void GlobalPurge() {
+    NSString *home = NSHomeDirectory();
     NSFileManager *fm = [NSFileManager defaultManager];
-    NSArray *targets = @[
+    NSArray *paths = @[
         @"Library/Caches/com.tencent.ig",
-        @"Library/WebKit", 
+        @"Library/Logs",
         @"Documents/ShadowTrackerExtra/Saved/Logs",
-        CLOUD_PATH,
-        STAT_PATH
+        @"Documents/ShadowTrackerExtra/Saved/StatCache",
+        @"Documents/ShadowTrackerExtra/Saved/CloudStorage",
+        @"tmp"
     ];
-    for (NSString *t in targets) {
-        [fm removeItemAtPath:[h stringByAppendingPathComponent:t] error:nil];
+    for (NSString *p in paths) {
+        [fm removeItemAtPath:[home stringByAppendingPathComponent:p] error:nil];
     }
 }
 
-// تخطي فحص الذاكرة الجديد (Anti-Memory Scan)
-void ProtectMemory() {
-    // استهداف محرك Turing الجديد الذي أُضيف في التحديث
-    Class turing = objc_getClass("TuringMessenger");
-    if (turing) {
-        Method m = class_getInstanceMethod(turing, sel_registerName("sendMessage:"));
-        if (m) method_setImplementation(m, class_getMethodImplementation([NewSecurityBypass class], @selector(stop)));
-    }
-}
+__attribute__((constructor)) static void core_entry() {
+    // تنظيف أولي صامت قبل بدء الجلسة
+    GlobalPurge();
 
-__attribute__((constructor)) static void final_shield() {
-    // مسح فوري للملفات قبل بدء اتصال السيرفر
-    SilentPurge();
-
-    // تأخير الحقن لـ 65 ثانية لضمان استقرار اللعبة وتخطي فحص اللوبي
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(65 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    // تم تعديل التأخير هنا ليكون 10 ثوانٍ فقط بناءً على طلبك
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        ProtectMemory();
-        
-        // تزييف هوية الجهاز بأسلوب الـ Block لمنع الكشف
+        // تزييف الهوية والنظام
         Method idfv = class_getInstanceMethod([UIDevice class], @selector(identifierForVendor));
         method_setImplementation(idfv, imp_implementationWithBlock(^(id self) {
-            return [[NSUUID alloc] initWithUUIDString:[[NSUUID UUID] UUIDString]];
+            return [[NSUUID alloc] initWithUUIDString:[[BlackAmarShield alloc] fetchNewID]];
         }));
 
-        // دورة تنظيف فائقة السرعة كل 10 ثوانٍ لمواجهة سرعة التحديث
-        [NSTimer scheduledTimerWithTimeInterval:10.0 repeats:YES block:^(NSTimer *timer) {
-            SilentPurge();
+        Method ver = class_getInstanceMethod([UIDevice class], @selector(systemVersion));
+        method_setImplementation(ver, imp_implementationWithBlock(^(id self) {
+            return @"12.1"; 
+        }));
+
+        // مؤقت تنظيف دوري كل 15 ثانية لمواجهة الحماية
+        [NSTimer scheduledTimerWithTimeInterval:15.0 repeats:YES block:^(NSTimer *timer) {
+            GlobalPurge();
         }];
+        
+        NSLog(@"[BLACK AMAR] PROTECTION ACTIVE - 10s DELAY APPLIED.");
     });
 }
